0af09b4f8eaaa88ea50356e441abb751de8c291e
Jhonatan Andrade <xdjhonatan0@gmail.com>
2020-06-18 01:29:29 -0300

    feat: create treme

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ThemeProvider {...{ theme }}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+export default {
+  colors: {},
+  roundness: {
+    xs: 4,
+    sm: 8,
+    md: 18,
+    lg: 28,
+  },
+  shadow: {
+    xs: 2,
+    sm: 8,
+    md: 22,
+    lg: 32,
+  },
+};

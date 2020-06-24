bb879ee3bfc6f084aeb6fcb7560316420411ddf2
Jhonatan Andrade <xdjhonatan0@gmail.com>
2020-06-24 03:21:05 -0300

    feat: create screens empty

---
+++ src/screens/Graphic/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Graphic = () => {
+  return <View />;
+};
+
+export default Graphic;
---
+++ src/screens/Time/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Time = () => {
+  return <View />;
+};
+
+export default Time;

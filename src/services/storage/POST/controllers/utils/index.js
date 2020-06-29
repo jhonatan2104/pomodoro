89c3693df01f2cc367a128aea857310dd8a9f87c
Jhonatan Andrade <xdjhonatan0@gmail.com>
2020-06-28 22:58:03 -0300

    feat: add validation config

--- src/services/storage/POST/controllers/utils/index.js
+++ src/services/storage/POST/controllers/utils/index.js
@@ -1,4 +1,19 @@
 /* eslint-disable no-prototype-builtins */
+
+export const validationPropConfig = (config) => {
+  if (config instanceof Object) {
+    const propsValid = [
+      config.hasOwnProperty('duration'),
+      config.hasOwnProperty('long_inteval'),
+      config.hasOwnProperty('short_inteval'),
+      config.hasOwnProperty('cicle_size'),
+    ];
+
+    return propsValid.every((res) => res);
+  }
+  return false;
+};
+
 export const validationPropPomodoro = (pomodoro) => {
   if (pomodoro instanceof Object) {
     const propsValid = [
@@ -6,6 +21,7 @@ export const validationPropPomodoro = (pomodoro) => {
       pomodoro.hasOwnProperty('config'),
       pomodoro.hasOwnProperty('log'),
       pomodoro.hasOwnProperty('icon'),
+      validationPropConfig(pomodoro.config),
     ];
 
     return propsValid.every((res) => res);

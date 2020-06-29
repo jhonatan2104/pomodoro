7e39596999aa49c18e84bac104a1f86ca0c76083
punisher077 <arthur.mts@gmail.com>
2020-06-29 17:03:04 -0300

    refactor: 💡 improve task component style

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Box, Label } from '..';
-import { Container, Tag } from './styles';
+// eslint-disable-next-line prettier/prettier
+import {
+  Container, Tag, Wrapper, CompleteContainer, Icon
+} from './styles';
 
 import theme from '~/theme';
 
-const Task = () => {
+const Task = (props) => {
+  const [checked, setChecked] = useState(false);
   return (
     <Box
-      mr={16}
-      ml={16}
       roundedSm
       row
+      height={80}
+      alignCenter
       justifyBetween
-      bg="#5D676F40"
+      bg={theme.colors.components.primary}
       paddingVertical={16}
       paddingHorizontal={12}
-      alignCenter
+      {...props}
     >
-      <Box row alignCenter>
+      <Wrapper>
         <Tag
           backgroundColor={`${theme.colors.importancy.red}20`}
           borderColor={theme.colors.importancy.red}
         />
-
         <Container>
           <Label color="#fff" size={18}>
             Task name
@@ -31,10 +35,39 @@ const Task = () => {
             30 minutes
           </Label>
         </Container>
-      </Box>
-      <Tag backgroundColor="#c4c4c420" />
+      </Wrapper>
+
+      <CompleteContainer>
+        <Tag
+          backgroundColor="#c4c4c420"
+          activeOpacity={1}
+          onPress={() => {
+            setChecked(!checked);
+          }}
+        >
+          <Icon
+            style={{ display: checked ? 'flex' : 'none' }}
+            name="check"
+            size={32}
+          />
+        </Tag>
+      </CompleteContainer>
     </Box>
   );
+};
+
+Task.propTypes = {
+  ml: PropTypes.oneOfType([PropTypes.number]),
+  mr: PropTypes.oneOfType([PropTypes.number]),
+  mt: PropTypes.oneOfType([PropTypes.number]),
+  mb: PropTypes.oneOfType([PropTypes.number]),
+};
+
+Task.defaultProps = {
+  mt: 0,
+  mb: 0,
+  mr: 0,
+  ml: 0,
 };
 
 export default Task;

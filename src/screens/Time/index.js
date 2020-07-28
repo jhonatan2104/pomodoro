b1b126d7e072050b34d65e064ce91ed75876bf22
Hércules de Sousa <herculesdeso@gmail.com>
2020-07-28 16:23:44 -0300

    style: placing buttons in right position

--- src/screens/Time/index.js
+++ src/screens/Time/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Box, CircularProgress } from '../../components';
+import { timing } from 'react-native-redash';
+import Animated, { Easing } from 'react-native-reanimated';
+import { Box, CircularProgress, Button } from '../../components';
+import styles from './styles';
+
+// Constants
+
+const { Clock } = Animated;
+const clock = new Clock();
+
+const timingConfig = timing({
+  clock,
+  duration: 5 * 1000,
+  from: 0,
+  to: 1,
+  easing: Easing.linear,
+});
 
 // import { Container } from './styles';
 
 const Time = () => {
   return (
-    <Box bg="#000" width="100%" height="100%">
-      <Text>asdfasdf</Text>
-      <CircularProgress />
-      <Box bg="#ffa" row self="center" width={150}>
-        <Text>asdfasdf</Text>
+    <Box width="100%" height="100%">
+      <CircularProgress progress={timingConfig} />
+      <Box
+        row
+        self="center"
+        width="100%"
+        justifyAround
+        style={styles.buttonContainer}
+      >
+        <Button mode="dark" width={152}>
+          <Text>Stop</Text>
+        </Button>
+        <Button mode="light" width={152}>
+          <Text>Pause</Text>
+        </Button>
       </Box>
     </Box>
   );
---
+++ src/screens/Time/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 8,
+  },
+});
+
+export default styles;
